de78cd52afb87bc634275d01399aff6438a5b43e
One Pieoms <sellrnzz@gmail.com>
2025-06-26 23:50:21 +0700

    Delete app/head.tsx

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function CustomHead() {
-  return (
-    <>
-      <meta name="95714a6c65a8ac999686234344e9d5786ecf727c" content="95714a6c65a8ac999686234344e9d5786ecf727c" />
-      <meta name="referrer" content="no-referrer-when-downgrade" />
-    </>
-  );
-}
